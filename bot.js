2395594bb13c11d2238739b52cf4962561762566
Plato-kun <55545424+Platokun@users.noreply.github.com>
2023-11-13 12:10:39 -0500

    i cba temp command handler disabled
    , need fix 2 converters

--- bot.js
+++ bot.js
@@ -1,23 +1,57 @@
+const fs = require('node:fs');
 const path = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const { loadCommands, clearCommands } = require('./commandHandler');
+const { token, clientId, guildId } = require('./config.json');
+
+// Initialize REST API
+const commands = [];
+const rest = new REST({ version: '9' }).setToken(token);
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
-const commands = [];
 
+client.commands = new Collection();
+process.env.PATH = "C:/Users/admin/AppData/Local/Programs/Python/Python311/python.exe;" + process.env.PATH;
+// define functions coz im a python user
+function loadCommands(dir) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const filePath = path.join(dir, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            loadCommands(filePath);
+        } else if (file.endsWith('.js')) {
+            const command = require(filePath);
+            console.log(`Loaded command: ${command.data.name} - ${command.data.description}`);
+            client.commands.set(command.data.name, command);
+            commands.push(command.data.toJSON());
+        }
+    }
+}
+//basicaly required to use this bot if you intend on adding or removing or editing commands
+function clearCommands() {
+    //base delete arguments for guild and global commands
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+    .then(() => console.log('Successfully deleted all guild commands.'))
+    .catch(console.error);
+
+    // for global commands
+    rest.put(Routes.applicationCommands(clientId), { body: [] })
+    .then(() => console.log('Successfully deleted all application commands.'))
+    
+    .catch(console.error);
+};
 client.once(Events.ClientReady, async () => {
     console.log('Ready!');
+    // basically reload discord's cache so u can use your new or editied commands
     clearCommands();
-    loadCommands(path.join(__dirname, 'commands'), commands, client);
+    // this actuialy sends the commands to discord's (the company's api servers) bot cache so you can actuially use new commands
+    // figuring out this actuially pissed me off
+    loadCommands(path.join(__dirname, 'commands'));
 
+    //handshakes, and loads the commands for Discord
     try {
-        const { REST } = require('@discordjs/rest');
-        const { Routes } = require('discord-api-types/v9');
-        const { clientId, guildId } = require('./config.json');
-        const rest = new REST({ version: '9' }).setToken(token);
-
         await rest.put(
             Routes.applicationGuildCommands(clientId, guildId),
             { body: commands },
--- commandHandler.js
+++ commandHandler.js
@@ -3,8 +3,26 @@ const path = require('path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token, clientId, guildId } = require('./config.json');
-
+//this file is used in bot.js
+/*original load commands function from bot.js that works
+function loadCommands(dir) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const filePath = path.join(dir, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            loadCommands(filePath);
+        } else if (file.endsWith('.js')) {
+            const command = require(filePath);
+            console.log(`Loaded command: ${command.data.name} - ${command.data.description}`);
+            client.commands.set(command.data.name, command);
+            commands.push(command.data.toJSON());
+        }
+    }
+}
+*/
 // Load commands from the specified directory
+/*broken load command function
 function loadCommands(commandsDir, commands, client) {
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
@@ -16,9 +34,25 @@ function loadCommands(commandsDir, commands, client) {
         commands.push(command.data.toJSON());
     }
 }
-
+*/
+//attempt 2 in loading commands
+function loadcommands(dir) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const filePath = path.join(dir, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            loadcommands(filePath);
+        } else if (file.endsWith('.js')) {
+            const command = require(filePath);
+            console.log(`Loaded command: ${command.data.name} - ${command.data.description}`);
+            client.commands.set(command.data.name, command);
+            commands.push(command.data.toJSON());
+        }
+    }
+}
 // Clear existing commands
-function clearCommands() {
+function clearcommands() {
     const rest = new REST({ version: '9' }).setToken(token);
 
     // Clear guild-specific commands
@@ -32,4 +66,4 @@ function clearCommands() {
         .catch(console.error);
 }
 
-module.exports = { loadCommands, clearCommands };
+module.exports = { loadcommands, clearcommands };
