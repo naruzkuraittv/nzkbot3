a6bdb8cc7d6042f07d7c761d451008a423d31307
Plato-kun <55545424+Platokun@users.noreply.github.com>
2024-07-26 23:08:08 -0400

    fixed syntaxical issues preventing  running application

--- commands/pokemon/typeroulette.js
+++ commands/pokemon/typeroulette.js
@@ -1,26 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 function random_type() {
-    const types = [normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy];
-    let chozentype = '';
-    while (chozentype === '') {
-        //pick a random type from the list 
-        chozentype = types[Math.floor(Math.random(), types.length)];
-    }
-    return chozentype;
+    const types = ['normal', 'fire', 'water', 'grass', 'electric', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'];
+    return types[Math.floor(Math.random() * types.length)];
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('TypeRoulette')
+        .setName('typeroulette')  
         .setDescription('Responds with a random pokemon type'),
     async execute(interaction) {
-         //3% chance to send the special message
-        if (Math.random() < 0.1) {
-            await interaction.reply(shiny  + random_type()); 
+        if (Math.random() < 0.03) {  
+            await interaction.reply('shiny ' + random_type());
         } else {
             await interaction.reply(random_type());
         }
     },
 };
-
